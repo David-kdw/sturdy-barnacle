0cdc5a6bc98dd2f8bf0cfdd1694138b87bae0d30
David-kdw <kudavids@yahoo.fr>
2024-07-03 18:19:13 +0000

    Première version de la fonction chebyshev_distance avec une fonction map

--- application.py
+++ application.py
@@ -1,15 +1,9 @@
 def chebyshev_distance(p1, p2):
     """Docstring..."""
-    max_diff = 0
-    for x, y in zip(p1, p2):
-        diff = abs(x - y)
-        if diff > max_diff:
-            max_diff = diff
-    return max_diff
+    return max(map(lambda x_y: abs(x_y[0] - x_y[1]), zip(p1, p2)))
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
     distance = chebyshev_distance(point1, point2)
     print("Chebyshev Distance:", distance)
-
