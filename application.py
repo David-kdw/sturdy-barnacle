58dd2efe68ac27c9bace74799db22d3f7f43fcc6
David-kdw <kudavids@yahoo.fr>
2024-07-03 18:18:07 +0000

    Première version de la fonction chebyshev_distance avec une boucle for

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 def chebyshev_distance(p1, p2):
     """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+    max_diff = 0
+    for x, y in zip(p1, p2):
+        diff = abs(x - y)
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
     point2 = (4, 5, 6)
     distance = chebyshev_distance(point1, point2)
     print("Chebyshev Distance:", distance)
+
